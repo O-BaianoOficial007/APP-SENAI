240aeb588265f3477addcbc804e949b071731f1c
O-BaianoOficial007 <arthurandradepereira7@gmail.com>
2026-08-13 11:16:36 -0300

    Add files via upload

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,14 @@
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 const image = {uri: 'https://legacy.reactjs.org/logo-og.png'}
 
 export default function Home() {
   return (
     <View style={s.container}>
-      <ImageBackground source={image} resizeMode="cover" style={s.image}/>
+      <ImageBackground source={require('../assets/images/image2.png')} resizeMode="cover" style={s.image}/>
+      <TouchableOpacity style={s.btn}>
+        <Text style={s.btnText}>ENTRAR</Text>
+      </TouchableOpacity>
       <Text>Index</Text>
     </View>
   );
@@ -18,4 +21,14 @@ const s = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
+  btn: {
+    position: 'absolute',
+    bottom: 100,
+    left: 50,
+  },
+  btnText:{
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 30
+  }
 });
