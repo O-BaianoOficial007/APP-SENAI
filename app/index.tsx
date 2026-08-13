0730959dcdca68b350746ed6b5607b9c454f4430
O-BaianoOficial007 <arthurandradepereira7@gmail.com>
2026-08-13 07:57:33 -0300

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Index</Text>
+    </View>
+  );
+}
+const s = StyleSheet.create({});
